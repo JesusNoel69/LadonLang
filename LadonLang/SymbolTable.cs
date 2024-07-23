09498bb1f44b404db83ca16b1bfc1ef9d0c705c0
Jesus <jnoel883@gmail.com>
2024-07-22 20:42:20 -0600

    Propiedad para el nombre de la estructura agregado y correccion de la vista de la tabla.

--- LadonLang/SymbolTable.cs
+++ LadonLang/SymbolTable.cs
@@ -1,26 +1,32 @@
 namespace LadonLang
 {
-    public class SymbolTable(string name, string scope, string offset, int size)
+    public class SymbolTable
     {
-        public string  Name{get; set;}=name;//required
-        public string? Type;
-        public string Scope=scope;//alcance     required
-        public string? DataType;
-        public string[]? Parameters;
-        public  string OffSet=offset;//required
-        public int Size = size; //required
-        public string? Context;
+        public string? Name{get; set;}="-";//required
+        public string? Type{get; set;}="-";
+        public string? Scope{get; set;}="-";//alcance     required
+        public string? DataType{get; set;}="-";
+        public List<string>? Parameters{get; set;}=[];
+        public  IntPtr? OffSet{get; set;}=null;//required
+        public int? Size{get; set;}=null; //required
+        public List<string>? Context{get; set;}=[];
+        public string? NameOfStructure{get; set;}="-";
         public static void ShowTable(List<SymbolTable> symbols)
         {
-            Console.WriteLine("----------------------------------------------------------------------------------------------------------");
-            Console.WriteLine("| Name       | Type       | Scope     | DataType   | OffSet  | Size   | Context       | Parameters        |");
-            Console.WriteLine("----------------------------------------------------------------------------------------------------------");
+            Console.WriteLine("-------------------------------------------------------------------------------------------------------------------------------------------------------------");
+            Console.WriteLine("| Name       | Type              | Scope     | DataType   | OffSet  | Size   | Context         | Parameters                     | Identifier of structure   |");
+            Console.WriteLine("-------------------------------------------------------------------------------------------------------------------------------------------------------------");
             foreach (var symbol in symbols)
             {
                 string parameters = symbol.Parameters != null ? string.Join(", ", symbol.Parameters) : "-";
-                Console.WriteLine($"| {symbol.Name,-10} | {symbol.Type ?? "-", -10} | {symbol.Scope, -9} | {symbol.DataType ?? "-", -10} | {symbol.OffSet, -7} | {symbol.Size, -6} | {symbol.Context ?? "-", -12} | {parameters, -18} |");
+                string context = "-";
+                symbol.Context?.ForEach((eachContext)=>{
+                    context+=eachContext+"->";
+                });
+                string? offset = symbol.OffSet != null ? symbol.OffSet.ToString() : "-";
+                Console.WriteLine($"| {symbol.Name,-10} | {symbol.Type ?? "-", -17} | {symbol.Scope, -9} | {symbol.DataType ?? "-", -10} | {offset, -7} | {symbol.Size, -6} | { context, -15} | {parameters, -30} | {symbol.NameOfStructure, -25} |");
             }
-            Console.WriteLine("----------------------------------------------------------------------------------------------------------");
+            Console.WriteLine("-------------------------------------------------------------------------------------------------------------------------------------------------------------");
         }
 
     }
