37fb04b971cc09456f3dd4325be5b87f74ff512b
Jesus <jnoel883@gmail.com>
2024-07-24 19:02:35 -0600

    Verificacion de variables, nombres de funcion e iidentiifiicadores de estructuras de control declaradas antes de ser usadas.

--- LadonLang/SemanticAnalizer.cs
+++ LadonLang/SemanticAnalizer.cs
@@ -1,3 +1,4 @@
+using System.Dynamic;
 using LadonLang.Data;
 
 namespace LadonLang
@@ -6,20 +7,82 @@ namespace LadonLang
     {
         private List<SymbolTable> symbolTable;
         private List<ASTNode> ast;
+        public List<string> DeclaratedVariableName=[];
         public SemanticAnalizer(ref List<SymbolTable> symbolTable, ref AstConstructor ast){
             this.symbolTable = symbolTable;
             this.ast = ast.root;
         }
         public void Analize(){
             foreach(ASTNode eachNode in ast){
-                eachNode.Print();
-                Console.WriteLine();
+                // eachNode.Print();
+                // Console.WriteLine();
+                TypeNode(eachNode);
+            }
+            SymbolTable.ShowTable(symbolTable);
+        }
+        public bool CreatedVariable(ASTNode eachNode){
+            bool exists = false;
+            UsageVariableNode? variable = eachNode as UsageVariableNode;
+            variable?.LeftValue?.ForEach(value=>{
+                if(value is Identifier){
+                    Identifier? nameOfValue = value as Identifier;
+                    DeclaratedVariableName.ForEach(eachVariable=>{
+                        if(nameOfValue?.Name?.ValueToken==eachVariable){
+                            exists = true;
+                        }
+                    });
+                }
+            });
+            variable?.LeftValue?.ForEach(value=>{
+                if(value is Identifier){
+                    Identifier? nameOfValue = value as Identifier;
+                    DeclaratedVariableName.ForEach(eachVariable=>{
+                        if(nameOfValue?.Name?.ValueToken==eachVariable){
+                            exists = true;
+                        }
+                    });
+                }
+            });
+            return exists;
+        }
+        public void TypeNode(ASTNode node){
+            if(node is DeclarationNode){
+                DeclarationNode? declaration = node as DeclarationNode;
+                DeclaratedVariableName.Add(declaration?.Identifier?.Name?.ValueToken??"");//
+            }else if(node is LoopNode){
+                LoopNode? loop = node as LoopNode;
+                if(loop?.Name?.ValueToken!=""){
+                    DeclaratedVariableName.Add(loop?.Name?.ValueToken??"");//
+                }
+                loop?.Block?.ForEach(block => {
+                    TypeNode(block);
+                });
+            }else if(node is IfNode){
+                IfNode? _if = node as IfNode;
+                _if?.IfBlock?.ForEach(block => {
+                    TypeNode(block);
+                });
+                if(_if?.Name?.ValueToken!=""){
+                    DeclaratedVariableName.Add(_if?.Name?.ValueToken??"");//
+                }
+            }else if(node is FunctionNode){
+                FunctionNode? function = node as FunctionNode;
+                function?.Block?.ForEach(block => {
+                    TypeNode(block);
+                });
+            }else if(node is EntityNode){
+                EntityNode? entity = node as EntityNode;
+                entity?.Block?.ForEach(block => {
+                    TypeNode(block);
+                });
+            }else if(node is UsageVariableNode){
+                if(!CreatedVariable(node)){
+                    throw new Exception("error. Variable no declarada previamente");
+                }
             }
         }
 
-        public bool DecratedVariable(){
-            return true;
-        }
+       
         public void Scope(){}
         public bool SameType(){
             return true;
